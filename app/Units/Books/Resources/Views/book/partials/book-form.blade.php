871f72cf2933db2e9f7904058b0b0b974f06cb74
Kelven G. Campos <kelven.campos@corp.saninternet.com>
2024-10-08 17:32:33 -0300

    test

--- app/Units/Books/Resources/Views/book/partials/book-form.blade.php
+++ app/Units/Books/Resources/Views/book/partials/book-form.blade.php
@@ -1,20 +1,6 @@
 @csrf
 
 <div>
-    @php
-        $bookAuthorsSelectedIds = old('author_ids', $book->authors()->pluck('CodAu')->toArray());
-    @endphp
-
-    <x-input-label for="author_ids" :value="__('Autores')" />
-    <x-select-input multiple id="author_ids[]" name="author_ids[]">
-        @foreach($authors as $author)
-            <option value="{{ $author->CodAu }}" {{ in_array($author->id, $bookAuthorsSelectedIds) ? 'selected' : '' }} >{{ $author->Nome }}</option>
-        @endforeach
-    </x-select-input>
-    <x-input-error class="mt-2" :messages="$errors->get('author_ids')" />
-</div>
-
-<div>
     <x-input-label for="Titulo" :value="__('Titulo')" />
     <x-text-input type="text" id="Titulo" name="Titulo"  value="{{ old('Titulo', $book->Titulo) }}" />
     <x-input-error class="mt-2" :messages="$errors->get('Titulo')" />
@@ -27,16 +13,42 @@
 </div>
 
 <div>
-    <x-input-label for="Edicao" :value="__('Edicao')" />
+    <x-input-label for="Edicao" :value="__('Edição')" />
     <x-text-input type="number" step="1" min="0"  id="Edicao" name="Edicao"  value="{{ old('Edicao', $book->Edicao) }}" oninput="this.value = this.value.replace(/[^0-9]/g, '')"  />
     <x-input-error class="mt-2" :messages="$errors->get('Edicao')" />
 </div>
 
+<div>
+    @php
+        $bookAuthorsSelectedIds = old('author_ids', $book->authors()->pluck('CodAu')->toArray());
+    @endphp
 
+    <x-input-label for="author_ids" :value="__('Autores')" />
+    <x-select-input multiple id="author_ids[]" name="author_ids[]">
+        @foreach($authors as $author)
+            <option value="{{ $author->CodAu }}" {{ in_array($author->CodAu, $bookAuthorsSelectedIds) ? 'selected' : '' }} >{{ $author->Nome }}</option>
+        @endforeach
+    </x-select-input>
+    <x-input-error class="mt-2" :messages="$errors->get('author_ids')" />
+</div>
 
 <div>
-    <x-input-label for="name" :value="__('Valor')" />
-    <x-text-input type="number" step=".01"  id="valor" name="valor"  value="{{ old('Valor', $book->Valor) }}" placeholder="0" />
+    @php
+        $bookSubjectSelectedIds = old('subject_ids', $book->authors()->pluck('CodAu')->toArray());
+    @endphp
+
+    <x-input-label for="subject_ids" :value="__('Assuntos')" />
+    <x-select-input multiple id="subject_ids[]" name="subject_ids[]">
+        @foreach($subjects as $subject)
+            <option value="{{ $subject->CodAs }}" {{ in_array($subject->CodAs, $bookSubjectSelectedIds) ? 'selected' : '' }} >{{ $subject->Descricao }}</option>
+        @endforeach
+    </x-select-input>
+    <x-input-error class="mt-2" :messages="$errors->get('subject_ids')" />
+</div>
+
+<div>
+    <x-input-label for="Valor" :value="__('Valor')" />
+    <x-text-input type="number" step=".01"  id="Valor" name="Valor"  value="{{ old('Valor', $book->Valor) }}" placeholder="0" />
     <x-input-error class="mt-2" :messages="$errors->get('Valor')" />
 </div>
 
